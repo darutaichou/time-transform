9352e21e25324d46a94c0626170979f53f34f803
dahi0104 <adgjmptw12behkilorvy@gmail.com>
2021-01-10 23:43:32 +0900

    TimeTransformサービス作成

---
+++ src/main/java/jp/co/bhopari/timetransform/services/TimeTransform.java
@@ -0,0 +1,22 @@
+package jp.co.bhopari.timetransform.services;
+
+/**
+ *
+ * @author adgjm
+ * 何時間何分を何分に変換する機能を提供するサービス
+ */
+
+public interface TimeTransform {
+
+	// 計算
+
+	/**
+	 *
+	 * @param hour 数値入力ボックスhourに入力された値
+	 * @param minute 数値入力ボックスminuteに入力された値
+	 * @return 変換結果
+	 * @throws IllegalArgumentException
+	 */
+
+	int transformTime(int hour, int minute) throws IllegalArgumentException;
+}
