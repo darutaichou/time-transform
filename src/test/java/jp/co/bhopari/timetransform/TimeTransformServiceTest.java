3c5ea50d4dcff1b430a06217fedd2335a466a231
dahi0104 <adgjmptw12behkilorvy@gmail.com>
2021-02-02 00:09:54 +0900

    境界値テストは完成

--- src/test/java/jp/co/bhopari/timetransform/TimeTransformServiceTest.java
+++ src/test/java/jp/co/bhopari/timetransform/TimeTransformServiceTest.java
@@ -26,7 +26,7 @@ public class TimeTransformServiceTest {
 
 	}
 
-	//入力値hourが0以上99未満の時、例外が発生しなければ成功
+	//入力値hourが0以上99未満の時、IllegalArgumentExceptionが発生しなければ成功
 	//入力値hourが0以上の境界値のテスト
 	@Test
 	public void 入力値hourが0以上99以下_0前後境界値Test() {
@@ -36,13 +36,23 @@ public class TimeTransformServiceTest {
 
 	}
 
-	//入力値hourが0以上99未満の時、例外が発生しなければ成功
+	//入力値hourが0以上99未満の時、IllegalArgumentExceptionが発生しなければ成功
 	//入力値hourが99以下の境界値のテスト
 	@Test
 	public void 入力値hourが0以上99以下_99前後境界値Test() {
 
 		//入力値hourを99にして時間表記変換メソッドを呼び出し、IllegalArgumentExceptionが発生しないことを確認
 		assertDoesNotThrow(() -> hourTestInstance.transformTime(99, ZERO_MINUTE));
+
+	}
+
+	//入力値hourが100以上の時、IllegalArgumentExceptionが発生すれば成功
+	//入力値hourが100以上の境界値のテスト
+	@Test
+	public void 入力値hourが100以上境界値Test() {
+
+		//入力値hourを100にして時間表記変換メソッドを呼び出し、IllegalArgumentExceptionが発生することを確認
+		assertThrows(IllegalArgumentException.class, () -> hourTestInstance.transformTime(100, ZERO_MINUTE));
 
 	}
 }
