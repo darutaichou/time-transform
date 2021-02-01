cd82597f02a50d0f15e51f9320eefc263643519e
dahi0104 <adgjmptw12behkilorvy@gmail.com>
2021-02-01 23:59:37 +0900

    99以下境界値テスト作成

--- src/test/java/jp/co/bhopari/timetransform/TimeTransformServiceTest.java
+++ src/test/java/jp/co/bhopari/timetransform/TimeTransformServiceTest.java
@@ -28,5 +28,21 @@ public class TimeTransformServiceTest {
 
 	//入力値hourが0以上99未満の時、例外が発生しなければ成功
 	//入力値hourが0以上の境界値のテスト
+	@Test
+	public void 入力値hourが0以上99以下_0前後境界値Test() {
 
+		//入力値hourを0にして時間表記変換メソッドを呼び出し、IllegalArgumentExceptionが発生しないことを確認
+		assertDoesNotThrow(() -> hourTestInstance.transformTime(0, ZERO_MINUTE));
+
+	}
+
+	//入力値hourが0以上99未満の時、例外が発生しなければ成功
+	//入力値hourが99以下の境界値のテスト
+	@Test
+	public void 入力値hourが0以上99以下_99前後境界値Test() {
+
+		//入力値hourを99にして時間表記変換メソッドを呼び出し、IllegalArgumentExceptionが発生しないことを確認
+		assertDoesNotThrow(() -> hourTestInstance.transformTime(99, ZERO_MINUTE));
+
+	}
 }
